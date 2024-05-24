747a232e295ba2acdc32380e75fd4088d5d15cdb
kwokho74 <kwokho@kwokho.com>
2024-05-24 07:36:26 -0600

    bug fix bsec2

--- micropython/modules/bsec2/bsec_wrapper.c
+++ micropython/modules/bsec2/bsec_wrapper.c
@@ -22,87 +22,87 @@ static mp_obj_t bsec_get_version_wrapper(void) {
 }
 static MP_DEFINE_CONST_FUN_OBJ_0(bsec_get_version_obj, bsec_get_version_wrapper);
 
-// Wrapper for bsec_init
-static mp_obj_t bsec_init_wrapper(void) {
-    bsec_library_return_t result = bsec_init();
-    if (result != BSEC_OK) {
-        mp_raise_msg(&mp_type_RuntimeError, "Failed to initialize BSEC library");
-    }
-    return mp_const_none;
-}
-static MP_DEFINE_CONST_FUN_OBJ_0(bsec_init_obj, bsec_init_wrapper);
+// // Wrapper for bsec_init
+// static mp_obj_t bsec_init_wrapper(void) {
+//     bsec_library_return_t result = bsec_init();
+//     if (result != BSEC_OK) {
+//         mp_raise_msg(&mp_type_RuntimeError, "Failed to initialize BSEC library");
+//     }
+//     return mp_const_none;
+// }
+// static MP_DEFINE_CONST_FUN_OBJ_0(bsec_init_obj, bsec_init_wrapper);
 
-// Wrapper for bsec_update_subscription
-static mp_obj_t bsec_update_subscription_wrapper(mp_obj_t requested_virtual_sensors_list, mp_obj_t required_sensor_settings_list) {
-    size_t n_requested_virtual_sensors;
-    mp_obj_t *requested_virtual_sensors_items;
-    mp_obj_get_array(requested_virtual_sensors_list, &n_requested_virtual_sensors, &requested_virtual_sensors_items);
+// // Wrapper for bsec_update_subscription
+// static mp_obj_t bsec_update_subscription_wrapper(mp_obj_t requested_virtual_sensors_list, mp_obj_t required_sensor_settings_list) {
+//     size_t n_requested_virtual_sensors;
+//     mp_obj_t *requested_virtual_sensors_items;
+//     mp_obj_get_array(requested_virtual_sensors_list, &n_requested_virtual_sensors, &requested_virtual_sensors_items);
 
-    size_t n_required_sensor_settings;
-    mp_obj_t *required_sensor_settings_items;
-    mp_obj_get_array(required_sensor_settings_list, &n_required_sensor_settings, &required_sensor_settings_items);
+//     size_t n_required_sensor_settings;
+//     mp_obj_t *required_sensor_settings_items;
+//     mp_obj_get_array(required_sensor_settings_list, &n_required_sensor_settings, &required_sensor_settings_items);
 
-    bsec_sensor_configuration_t requested_virtual_sensors[n_requested_virtual_sensors];
-    bsec_sensor_configuration_t required_sensor_settings[BSEC_MAX_PHYSICAL_SENSOR];
+//     bsec_sensor_configuration_t requested_virtual_sensors[n_requested_virtual_sensors];
+//     bsec_sensor_configuration_t required_sensor_settings[BSEC_MAX_PHYSICAL_SENSOR];
     
-    for (size_t i = 0; i < n_requested_virtual_sensors; i++) {
-        mp_obj_t *tuple;
-        mp_obj_get_array_fixed_n(requested_virtual_sensors_items[i], 2, &tuple);
-        requested_virtual_sensors[i].sensor_id = mp_obj_get_int(tuple[0]);
-        requested_virtual_sensors[i].sample_rate = mp_obj_get_float(tuple[1]);
-    }
+//     for (size_t i = 0; i < n_requested_virtual_sensors; i++) {
+//         mp_obj_t *tuple;
+//         mp_obj_get_array_fixed_n(requested_virtual_sensors_items[i], 2, &tuple);
+//         requested_virtual_sensors[i].sensor_id = mp_obj_get_int(tuple[0]);
+//         requested_virtual_sensors[i].sample_rate = mp_obj_get_float(tuple[1]);
+//     }
 
-    uint8_t n_required_sensor_settings_out = BSEC_MAX_PHYSICAL_SENSOR;
-    bsec_library_return_t result = bsec_update_subscription(requested_virtual_sensors, n_requested_virtual_sensors, required_sensor_settings, &n_required_sensor_settings_out);
-    if (result != BSEC_OK) {
-        mp_raise_msg(&mp_type_RuntimeError, "Failed to update BSEC subscription");
-    }
+//     uint8_t n_required_sensor_settings_out = BSEC_MAX_PHYSICAL_SENSOR;
+//     bsec_library_return_t result = bsec_update_subscription(requested_virtual_sensors, n_requested_virtual_sensors, required_sensor_settings, &n_required_sensor_settings_out);
+//     if (result != BSEC_OK) {
+//         mp_raise_msg(&mp_type_RuntimeError, "Failed to update BSEC subscription");
+//     }
 
-    mp_obj_t output_list = mp_obj_new_list(n_required_sensor_settings_out, NULL);
-    for (uint8_t i = 0; i < n_required_sensor_settings_out; i++) {
-        mp_obj_t tuple[2];
-        tuple[0] = mp_obj_new_int(required_sensor_settings[i].sensor_id);
-        tuple[1] = mp_obj_new_float(required_sensor_settings[i].sample_rate);
-        mp_obj_list_store(output_list, i, mp_obj_new_tuple(2, tuple));
-    }
+//     mp_obj_t output_list = mp_obj_new_list(n_required_sensor_settings_out, NULL);
+//     for (uint8_t i = 0; i < n_required_sensor_settings_out; i++) {
+//         mp_obj_t tuple[2];
+//         tuple[0] = mp_obj_new_int(required_sensor_settings[i].sensor_id);
+//         tuple[1] = mp_obj_new_float(required_sensor_settings[i].sample_rate);
+//         mp_obj_list_store(output_list, i, mp_obj_new_tuple(2, tuple));
+//     }
 
-    return output_list;
-}
-static MP_DEFINE_CONST_FUN_OBJ_2(bsec_update_subscription_obj, bsec_update_subscription_wrapper);
+//     return output_list;
+// }
+// static MP_DEFINE_CONST_FUN_OBJ_2(bsec_update_subscription_obj, bsec_update_subscription_wrapper);
 
-// Wrapper for bsec_do_steps
-// bsec_library_return_t bsec_do_steps(const bsec_input_t * const inputs, const uint8_t n_inputs, bsec_output_t * outputs, uint8_t * n_outputs);
-static mp_obj_t bsec_do_steps_wrapper(mp_obj_t inputs_list) {
-    size_t n_inputs;
-    mp_obj_t *inputs_items;
-    mp_obj_get_array(inputs_list, &n_inputs, &inputs_items);
+// // Wrapper for bsec_do_steps
+// // bsec_library_return_t bsec_do_steps(const bsec_input_t * const inputs, const uint8_t n_inputs, bsec_output_t * outputs, uint8_t * n_outputs);
+// static mp_obj_t bsec_do_steps_wrapper(mp_obj_t inputs_list) {
+//     size_t n_inputs;
+//     mp_obj_t *inputs_items;
+//     mp_obj_get_array(inputs_list, &n_inputs, &inputs_items);
 
-    bsec_input_t inputs[n_inputs];
-    for (size_t i = 0; i < n_inputs; i++) {
-        mp_obj_t *tuple;
-        mp_obj_get_array_fixed_n(inputs_items[i], 2, &tuple);
-        inputs[i].sensor_id = mp_obj_get_int(tuple[0]);
-        inputs[i].signal = mp_obj_get_float(tuple[1]);
-    }
+//     bsec_input_t inputs[n_inputs];
+//     for (size_t i = 0; i < n_inputs; i++) {
+//         mp_obj_t *tuple;
+//         mp_obj_get_array_fixed_n(inputs_items[i], 2, &tuple);
+//         inputs[i].sensor_id = mp_obj_get_int(tuple[0]);
+//         inputs[i].signal = mp_obj_get_float(tuple[1]);
+//     }
 
-    bsec_output_t outputs[BSEC_NUMBER_OUTPUTS];
-    uint8_t n_outputs;
-    bsec_library_return_t result = bsec_do_steps(inputs, n_inputs, outputs, &n_outputs);
-    if (result != BSEC_OK) {
-        mp_raise_msg(&mp_type_RuntimeError, "Failed to run BSEC steps");
-    }
+//     bsec_output_t outputs[BSEC_NUMBER_OUTPUTS];
+//     uint8_t n_outputs;
+//     bsec_library_return_t result = bsec_do_steps(inputs, n_inputs, outputs, &n_outputs);
+//     if (result != BSEC_OK) {
+//         mp_raise_msg(&mp_type_RuntimeError, "Failed to run BSEC steps");
+//     }
 
-    mp_obj_t output_list = mp_obj_new_list(n_outputs, NULL);
-    for (uint8_t i = 0; i < n_outputs; i++) {
-        mp_obj_t tuple[2];
-        tuple[0] = mp_obj_new_int(outputs[i].sensor_id);
-        tuple[1] = mp_obj_new_float(outputs[i].signal);
-        mp_obj_list_store(output_list, i, mp_obj_new_tuple(2, tuple));
-    }
+//     mp_obj_t output_list = mp_obj_new_list(n_outputs, NULL);
+//     for (uint8_t i = 0; i < n_outputs; i++) {
+//         mp_obj_t tuple[2];
+//         tuple[0] = mp_obj_new_int(outputs[i].sensor_id);
+//         tuple[1] = mp_obj_new_float(outputs[i].signal);
+//         mp_obj_list_store(output_list, i, mp_obj_new_tuple(2, tuple));
+//     }
 
-    return output_list;
-}
-static MP_DEFINE_CONST_FUN_OBJ_1(bsec_do_steps_obj, bsec_do_steps_wrapper);
+//     return output_list;
+// }
+// static MP_DEFINE_CONST_FUN_OBJ_1(bsec_do_steps_obj, bsec_do_steps_wrapper);
 
 // Define all attributes of the module.
 // Table entries are key/value pairs of the attribute name (a string)
