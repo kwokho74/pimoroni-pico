8462c67d70d0c2116d863093e1a446280d733564
kwokho74 <kwokho@kwokho.com>
2024-05-27 17:29:21 -0600

    added bsec_do_steps

--- micropython/modules/bsec2/bsec_wrapper.c
+++ micropython/modules/bsec2/bsec_wrapper.c
@@ -167,9 +167,6 @@ static mp_obj_t sensor_settings_to_dict(const bsec_bme_settings_t *settings) {
 static mp_obj_t bsec_sensor_control_wrapper(mp_obj_t timestamp_obj) {
     int64_t timestamp = (int64_t)mp_obj_get_int(timestamp_obj);
 
-    // Print the timestamp for debugging purposes
-    mp_printf(&mp_plat_print, "Timestamp: %ld\n", timestamp);
-
     bsec_bme_settings_t sensor_settings;
 
     bsec_library_return_t result = bsec_sensor_control(timestamp, &sensor_settings);
@@ -180,6 +177,52 @@ static mp_obj_t bsec_sensor_control_wrapper(mp_obj_t timestamp_obj) {
     return sensor_settings_to_dict(&sensor_settings);
 }
 static MP_DEFINE_CONST_FUN_OBJ_1(bsec_sensor_control_obj, bsec_sensor_control_wrapper);
+
+// Wrapper for bsec_do_steps
+static mp_obj_t bsec_do_steps_wrapper(mp_obj_t inputs_tuple) {
+    mp_obj_t *inputs;
+    size_t n_inputs;
+    
+    // Get the tuples
+    mp_obj_get_array(inputs_tuple, &n_inputs, &inputs);
+    
+    bsec_input_t bsec_inputs[n_inputs];
+    for (size_t i = 0; i < n_inputs; i++) {
+        mp_obj_t *items;
+        size_t len;
+        mp_obj_get_array(inputs[i], &len, &items);
+        if (len != 3) {
+            mp_raise_ValueError("Input tuple length must be 3");
+        }
+
+        bsec_inputs[i].sensor_id = mp_obj_get_int(items[0]);
+        bsec_inputs[i].signal = mp_obj_get_float(items[1]);
+        bsec_inputs[i].time_stamp = mp_obj_get_int(items[2]);
+    }
+
+    bsec_output_t outputs[BSEC_NUMBER_OUTPUTS];
+    uint8_t n_outputs = BSEC_NUMBER_OUTPUTS;
+
+    bsec_library_return_t result = bsec_do_steps(bsec_inputs, n_inputs, outputs, &n_outputs);
+
+    // Convert outputs to list of dictionaries
+    mp_obj_t outputs_list = mp_obj_new_list(n_outputs, NULL);
+    for (size_t i = 0; i < n_outputs; i++) {
+        mp_obj_t dict = mp_obj_new_dict(0);
+        mp_obj_dict_store(dict, MP_OBJ_NEW_QSTR(MP_QSTR_sensor_id), mp_obj_new_int(outputs[i].sensor_id));
+        mp_obj_dict_store(dict, MP_OBJ_NEW_QSTR(MP_QSTR_signal), mp_obj_new_float(outputs[i].signal));
+        mp_obj_dict_store(dict, MP_OBJ_NEW_QSTR(MP_QSTR_time_stamp), mp_obj_new_int(outputs[i].time_stamp));
+        mp_obj_list_store(outputs_list, i, dict);
+    }
+
+    // Return a tuple with result and outputs
+    mp_obj_t result_tuple[2];
+    result_tuple[0] = mp_obj_new_int(result);
+    result_tuple[1] = outputs_list;
+
+    return mp_obj_new_tuple(2, result_tuple);
+}
+static MP_DEFINE_CONST_FUN_OBJ_1(bsec_do_steps_obj, bsec_do_steps_wrapper);
 
 // Define all attributes of the module.
 // Table entries are key/value pairs of the attribute name (a string)
@@ -195,6 +238,7 @@ static const mp_rom_map_elem_t bsec2_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_set_state), MP_ROM_PTR(&bsec_set_state_obj) },
     { MP_ROM_QSTR(MP_QSTR_update_subscription), MP_ROM_PTR(&bsec_update_subscription_obj) },
     { MP_ROM_QSTR(MP_QSTR_sensor_control), MP_ROM_PTR(&bsec_sensor_control_obj) },
+    { MP_ROM_QSTR(MP_QSTR_do_steps), MP_ROM_PTR(&bsec_do_steps_obj) },
 };
 
 static MP_DEFINE_CONST_DICT(bsec_module_globals, bsec2_module_globals_table);
