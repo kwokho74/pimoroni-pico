948fe097f335145a8ed758c5e72575049b42f929
kwokho74 <kwokho@kwokho.com>
2024-05-29 00:24:07 -0600

    handle int64

--- micropython/modules/bsec2/bsec_wrapper.c
+++ micropython/modules/bsec2/bsec_wrapper.c
@@ -42,7 +42,8 @@ static mp_obj_t sensor_configuration_to_tuple(const bsec_sensor_configuration_t 
 // Helper function to convert bsec_bme_settings_t to dictionary
 static mp_obj_t sensor_settings_to_dict(const bsec_bme_settings_t *settings) {
     mp_obj_t dict = mp_obj_new_dict(0);
-    mp_obj_dict_store(dict, MP_OBJ_NEW_QSTR(MP_QSTR_next_call), mp_obj_new_int(settings->next_call));
+    mp_obj_dict_store(dict, MP_OBJ_NEW_QSTR(MP_QSTR_next_call_high), mp_obj_new_int(settings->next_call >> 32 & 0xFFFFFFFF));
+    mp_obj_dict_store(dict, MP_OBJ_NEW_QSTR(MP_QSTR_next_call_low), mp_obj_new_int(settings->next_call & 0xFFFFFFFF));
     mp_obj_dict_store(dict, MP_OBJ_NEW_QSTR(MP_QSTR_process_data), mp_obj_new_int(settings->process_data));
     mp_obj_dict_store(dict, MP_OBJ_NEW_QSTR(MP_QSTR_heater_temperature), mp_obj_new_int(settings->heater_temperature));
     mp_obj_dict_store(dict, MP_OBJ_NEW_QSTR(MP_QSTR_heater_duration), mp_obj_new_int(settings->heater_duration));
@@ -163,21 +164,24 @@ static mp_obj_t bsec_update_subscription_wrapper(mp_obj_t requested_virtual_sens
 static MP_DEFINE_CONST_FUN_OBJ_1(bsec_update_subscription_obj, bsec_update_subscription_wrapper);
 
 // Wrapper for bsec_sensor_control
-static mp_obj_t bsec_sensor_control_wrapper(mp_obj_t timestamp_obj) {
-    mp_printf(&mp_plat_print, "timestamp_obj1");
-    int64_t timestamp = (int64_t)mp_obj_get_int(timestamp_obj);
-    mp_printf(&mp_plat_print, "timestamp_obj2");
+static mp_obj_t bsec_sensor_control_wrapper(mp_obj_t time_stamp_high_obj, mp_obj_t time_stamp_low_obj) {
+    mp_printf(&mp_plat_print, "timestamp_obj1\n");
+    uint32_t time_stamp_high = (uint32_t)mp_obj_get_int(time_stamp_high_obj);
+    mp_printf(&mp_plat_print, "timestamp_high: %u\n", time_stamp_high);
+    uint32_t time_stamp_low = (uint32_t)mp_obj_get_int(time_stamp_low_obj);
+    mp_printf(&mp_plat_print, "timestamp_low: %u\n", time_stamp_low);
+    int64_t time_stamp = ((int64_t)time_stamp_high << 32) | time_stamp_low;
+    mp_printf(&mp_plat_print, "timestamp_obj4\n");
 
     bsec_bme_settings_t sensor_settings;
-
-    bsec_library_return_t result = bsec_sensor_control(timestamp, &sensor_settings);
+    bsec_library_return_t result = bsec_sensor_control(time_stamp, &sensor_settings);
     if (result != BSEC_OK) {
         return to_result_tuple_none(result);
     }
 
     return to_result_tuple(result, sensor_settings_to_dict(&sensor_settings));
 }
-static MP_DEFINE_CONST_FUN_OBJ_1(bsec_sensor_control_obj, bsec_sensor_control_wrapper);
+static MP_DEFINE_CONST_FUN_OBJ_2(bsec_sensor_control_obj, bsec_sensor_control_wrapper);
 
 // Wrapper for bsec_do_steps
 static mp_obj_t bsec_do_steps_wrapper(mp_obj_t inputs_tuple) {
@@ -198,7 +202,7 @@ static mp_obj_t bsec_do_steps_wrapper(mp_obj_t inputs_tuple) {
 
         bsec_inputs[i].sensor_id = mp_obj_get_int(items[0]);
         bsec_inputs[i].signal = mp_obj_get_float(items[1]);
-        bsec_inputs[i].time_stamp = mp_obj_get_int(items[2]);
+        bsec_inputs[i].time_stamp = ((int64_t)mp_obj_get_int(items[2]) << 32) | mp_obj_get_int(items[3]);
     }
 
     bsec_output_t outputs[BSEC_NUMBER_OUTPUTS];
@@ -215,7 +219,8 @@ static mp_obj_t bsec_do_steps_wrapper(mp_obj_t inputs_tuple) {
         mp_obj_t dict = mp_obj_new_dict(0);
         mp_obj_dict_store(dict, MP_OBJ_NEW_QSTR(MP_QSTR_sensor_id), mp_obj_new_int(outputs[i].sensor_id));
         mp_obj_dict_store(dict, MP_OBJ_NEW_QSTR(MP_QSTR_signal), mp_obj_new_float(outputs[i].signal));
-        mp_obj_dict_store(dict, MP_OBJ_NEW_QSTR(MP_QSTR_time_stamp), mp_obj_new_int(outputs[i].time_stamp));
+        mp_obj_dict_store(dict, MP_OBJ_NEW_QSTR(MP_QSTR_time_stamp_high), mp_obj_new_int(outputs[i].time_stamp) >> 32 & 0xFFFFFFFF);
+        mp_obj_dict_store(dict, MP_OBJ_NEW_QSTR(MP_QSTR_time_stamp_low), mp_obj_new_int(outputs[i].time_stamp) & 0xFFFFFFFF);
         mp_obj_list_store(outputs_list, MP_OBJ_NEW_SMALL_INT(i), dict);
     }
 
