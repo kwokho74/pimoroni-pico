bb4cb517e299243b1afba172c9f8ecdff0e79ccb
kwokho74 <kwokho@kwokho.com>
2024-06-04 10:17:48 -0600

    fixed accuracy issus, return as int

--- micropython/modules/bsec2/bsec_wrapper.c
+++ micropython/modules/bsec2/bsec_wrapper.c
@@ -229,7 +229,7 @@ static mp_obj_t bsec_do_steps_wrapper(mp_obj_t inputs_tuple) {
         mp_obj_t dict = mp_obj_new_dict(0);
         mp_obj_dict_store(dict, MP_OBJ_NEW_QSTR(MP_QSTR_sensor_id), mp_obj_new_int(outputs[i].sensor_id));
         mp_obj_dict_store(dict, MP_OBJ_NEW_QSTR(MP_QSTR_signal), mp_obj_new_float(outputs[i].signal));
-        mp_obj_dict_store(dict, MP_OBJ_NEW_QSTR(MP_QSTR_accuracy), mp_obj_new_float(outputs[i].accuracy));
+        mp_obj_dict_store(dict, MP_OBJ_NEW_QSTR(MP_QSTR_accuracy), mp_obj_new_int(outputs[i].accuracy));
         mp_obj_dict_store(dict, MP_OBJ_NEW_QSTR(MP_QSTR_time_stamp_high), mp_obj_new_int_from_uint(to_uint_high(outputs[i].time_stamp)));
         mp_obj_dict_store(dict, MP_OBJ_NEW_QSTR(MP_QSTR_time_stamp_low), mp_obj_new_int_from_uint(to_uint_low(outputs[i].time_stamp)));
         mp_obj_list_store(outputs_list, MP_OBJ_NEW_SMALL_INT(i), dict);
